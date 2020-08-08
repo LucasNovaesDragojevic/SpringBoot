a25345389027b4bce6a7894d576b3a60c54b4ba3
LucasNovaesDragojevic <lucas.dragojevic@gmail.com>
2020-08-08 11:30:53 -0300

    Ativado Spring Security no projeto

---
+++ src/main/java/br/com/forum/config/security/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package br.com.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter
+{
+	/**
+	 * Configurações de autenticação
+	 */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+	}
+	
+	/**
+	 * Configurações de autorização
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+	}
+	
+	/**
+	 * Configurações de recursos estáticos(js, css, imagens, etc.)
+	 */
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+	}
+}
