d04102b6084fcaaad9b69f18094d9559379610dd
LucasNovaesDragojevic <lucas.dragojevic@gmail.com>
2020-08-08 12:08:29 -0300

    Implantada lógica de autenticação de usuário

--- src/main/java/br/com/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/forum/config/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,36 +8,48 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 {
+	@Autowired
+	private AutenticacaoService autenticacaoService;
+	
 	/**
 	 * Configurações de autenticação
 	 */
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception 
+	{
+		auth.userDetailsService(autenticacaoService)
+			.passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
 	/**
 	 * Configurações de autorização
 	 */
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	protected void configure(HttpSecurity http) throws Exception 
+	{
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.GET, "topicos")
 			.permitAll()
 			.antMatchers(HttpMethod.GET, "topicos/*")
-			.permitAll();
+			.permitAll()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.formLogin();
 	}
 	
 	/**
 	 * Configurações de recursos estáticos(js, css, imagens, etc.)
 	 */
 	@Override
-	public void configure(WebSecurity web) throws Exception {
+	public void configure(WebSecurity web) throws Exception 
+	{
 		
 	}
 }
--- src/main/java/br/com/forum/model/Usuario.java
+++ src/main/java/br/com/forum/model/Usuario.java
@@ -1,9 +1,17 @@
 package br.com.forum.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,12 +20,51 @@ import lombok.ToString;
 
 @Entity
 @Getter @Setter @ToString @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Usuario 
+public class Usuario implements UserDetails
 {
+	private static final long serialVersionUID = 1L;
+	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
 	private Long id;
 	private String nome;
 	private String email;
 	private String senha;
+	@ManyToMany(fetch = FetchType.EAGER)
+	private Collection<Perfil> perfis = new ArrayList<Perfil>();
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.perfis;
+	}
+	
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+	
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
